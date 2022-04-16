809834c842d458e818c4c3dcd3bc4a0b757234c4
Thosam1 <thosamtenzin2002@gmail.com>
2022-04-16 13:17:57 +0200

    image gallery to showcase my little sister artwork

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  // list of domains we allow optimization
+  images:{
+    domains: ['bit.ly', 'pbs.twimg.com', 'cdnb.artstation.com', 'cdna.artstation.com'],
+  }
 }
